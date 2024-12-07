10e57e4694310539061311c612f9fc10829db840
vivek-0115 <vivekraj0620@gmail.com>
2024-12-07 11:45:02 +0000

    New Page added.

---
+++ src/views/LoginView.vue
@@ -0,0 +1,45 @@
+<template>
+    <h1>Login</h1>
+    <div>
+      <input type="text" placeholder="Email" v-model="email"/> <br><br>
+      <input type="password" placeholder="Password" v-model="password"/> <br> <br>
+      <button @click="submitLogin">Login</button>
+    </div>
+</template>
+
+<script>
+export default {
+  name:"Login",
+
+  data(){
+    return{
+      email:'',
+      password:'',
+    }
+  },
+  methods:{
+      async submitLogin(){
+        const url = 'http://127.0.0.1:5000/login';
+        const userData = { 'email': this.email, 'password': this.password };
+
+        const response=await fetch(url, {
+          method:'POST',
+          headers: {'Content-Type': 'application/json',},
+          body: JSON.stringify(userData),
+        });
+
+        this.email=''
+        this.password=''        
+
+        if(response.ok){
+          const data=await response.json()
+          console.log(data);
+        }
+      }
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/RegisterView.vue
@@ -0,0 +1,48 @@
+<template>
+    <h1>Register</h1>
+    <div>
+      <input type="text" placeholder="Email" v-model="email"/> <br><br>
+      <input type="password" placeholder="Password" v-model="password"/> <br> <br>
+      <input type="text" placeholder="Role:user only" v-model="role"/> <br><br>
+      <button @click="registerLogin">Register</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Register",
+
+    data(){
+        return{
+            email:'',
+            password:'',
+            role:'',
+        }
+    },
+    methods:{
+        async registerLogin(){
+        const url = 'http://127.0.0.1:5000/register';
+        const userData = { 'email': this.email, 'password': this.password, 'role':this.role };
+
+        const response=await fetch(url, {
+          method:'POST',
+          headers: {'Content-Type': 'application/json',},
+          body: JSON.stringify(userData),
+        });
+
+        this.role='',
+        this.email=''
+        this.password=''
+
+        if(response.ok){
+          const data=await response.json()
+          console.log(data);
+        }
+      }
+    }
+}
+</script>
+
+<style>
+
+</style>
