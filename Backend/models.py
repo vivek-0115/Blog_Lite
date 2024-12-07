3f6fa5d313360654b9c96b7eb37034f67538fdf5
vivek-0115 <vivekraj0620@gmail.com>
2024-12-07 11:47:42 +0000

    Codes Updated

--- Backend/models.py
+++ Backend/models.py
@@ -1,23 +1,32 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 from flask_security import UserMixin, RoleMixin
 
 db = SQLAlchemy()
 
-class User(db.Model,UserMixin): # user
-    id = db.Column(db.Integer, primary_key = True)
-    email = db.Column(db.String, unique = True, nullable = False) #"1@1"
-    password = db.Column(db.String, nullable = False)
-    fs_uniquifier = db.Column(db.String, unique = True, nullable = False)
-    active = db.Column(db.Boolean, nullable = False)
-    roles = db.relationship('Role', backref = 'bearer', secondary = 'user_roles')
-    # more attributes can be added
-class Role(db.Model, RoleMixin): # role
-    # required attribute
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String, nullable = False, unique = True)
+class User(db.Model, UserMixin):  # user
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    fs_uniquifier = db.Column(db.String, unique=True, nullable=False)
+    active = db.Column(db.Boolean, nullable=False)
+    roles = db.relationship('Role', backref='bearer', secondary='user_roles')
+
+class Role(db.Model, RoleMixin):  # role
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False, unique=True)
     description = db.Column(db.String)
-    # more attributes can be added
-class UserRoles (db.Model): #users_roles
-    id = db.Column(db.Integer, primary_key = True)
+
+class UserRoles(db.Model):  # users_roles
+    id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
+
+class Blog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    caption = db.Column(db.Text, nullable=True)
+    image_url = db.Column(db.String(2083), nullable=True)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <Nav/>
   <router-view/>
 </template>
+
+<script>
+// @ is an alias to /src
+import Nav from '@/components/Nav.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Nav,
+  }
+}
+</script>
 
 <style>
 #app {
@@ -13,18 +22,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
