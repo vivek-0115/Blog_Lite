cb361ac6b85a79dc915f152f6ef1aba4782f941d
vivek-0115 <vivekraj0620@gmail.com>
2024-12-07 11:48:33 +0000

    Updated

--- src/router/index.js
+++ src/router/index.js
@@ -8,15 +8,22 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/login',
+    name: 'login',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+      return import(/* webpackChunkName: "about" */ '../views/LoginView.vue')
     }
-  }
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: function () {
+      return import('../views/RegisterView.vue')
+    }
+  },
 ]
 
 const router = createRouter({
